0c955d1529d13ea8c91cbe3af8c0b50e4cffb5ca
coz-j <coz-j@saint-louis29.net>
2026-01-29 15:04:19 +0100

    test nouvelle partie ok

--- src/sl29/games/_2048.py
+++ src/sl29/games/_2048.py
@@ -18,7 +18,10 @@ def nouvelle_partie() -> Tuple[List[List[int]], int]:
     :return: Une grille TAILLExTAILLE initialisée avec deux tuiles, ainsi que le score à 0.
     :rtype: Tuple[List[List[int]], int]
     """
-    raise NotImplementedError("Fonction nouvelle_partie non implémentée.")
+    grille = _creer_plateau_vide()
+    grille2 = _ajouter_tuile(grille)
+    grille3 =_ajouter_tuile(grille2)
+    return (grille3,0)
 
 def jouer_coup(plateau: List[List[int]], direction: str) -> tuple[List[List[int]], int, bool]:
     """
